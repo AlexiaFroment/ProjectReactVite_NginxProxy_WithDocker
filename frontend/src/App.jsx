f9d2099782da0b96e75781416824ada91249cc2d
AloaCmoi <aloacmoi@gmail.com>
2024-07-25 14:30:06 +0200

    Update frontend dockerfile with node:lts-alpine3.19

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 export const App = () => {
   return (
-    <div>Les poulettes sont dans le jardin & mangent des pommes vertes</div>
+    <div>
+      Les poulettes sont dans le jardin & mangent des pommes vertes avec des
+      pois bleus avec des vers dedans
+    </div>
   );
 };
